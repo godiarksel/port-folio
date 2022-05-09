fe1d90fb87c3c2a0f80256f1aa63cfc1dfc49336
godfrey <godiarksel@gmail.com>
2022-05-10 01:19:07 +0300

    Run linter test

--- myscript.js
+++ myscript.js
@@ -1,18 +1,16 @@
 const openMenu = document.querySelector('.openMenu');
 const mobBtn = document.querySelector('.mobile-navlist');
 
-
 openMenu.addEventListener('click', show);
 mobBtn.addEventListener('click', show);
 
-
-function show(){
+function show() {
   openMenu.style.display = 'none';
   mobBtn.style.left = '0';
 }
-function readmenu (num){
+
+function readmenu(num) {
   window.location.href = num;
   const mobBtn = document.querySelector('.mobile-navlist');
   mobBtn.style.display = 'none';
-  openMenu.style.display = ' ';
-  }
+}
