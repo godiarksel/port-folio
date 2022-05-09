de0368120ddddd0ffd19acae37a7bb0e797a20d3
godfrey <godiarksel@gmail.com>
2022-05-10 00:21:30 +0300

    Make minor modifications

--- myscript.js
+++ myscript.js
@@ -1,17 +1,18 @@
 const openMenu = document.querySelector('.openMenu');
 const mobBtn = document.querySelector('.mobile-navlist');
 
+
 openMenu.addEventListener('click', show);
 mobBtn.addEventListener('click', show);
-
 
 
 function show(){
   openMenu.style.display = 'none';
   mobBtn.style.left = '0';
 }
-function readmenu (){
-  window.location.href = '#work-section';
+function readmenu (num){
+  window.location.href = num;
   const mobBtn = document.querySelector('.mobile-navlist');
   mobBtn.style.display = 'none';
+  openMenu.style.display = ' ';
   }
