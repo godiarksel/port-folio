3d575d7cfa6d0838cde3c0c2e3b47f1f1432d4c6
godfrey <godiarksel@gmail.com>
2022-05-10 05:48:46 +0300

    Fix linter errors

--- myscript.js
+++ myscript.js
@@ -1,16 +1,19 @@
-const openMenu = document.querySelector('.openMenu');
 const mobBtn = document.querySelector('.mobile-navlist');
+const linkBtn = document.querySelector('.mobile-navlist a');
 
-openMenu.addEventListener('click', show);
-mobBtn.addEventListener('click', show);
+function readmenu(num) {
+  window.location.href = num;
+  // const mobBtn = document.querySelector('.mobile-navlist');
+  mobBtn.style.display = 'none';
+}
+
+const openMenu = document.querySelector('.openMenu');
 
 function show() {
   openMenu.style.display = 'none';
   mobBtn.style.left = '0';
 }
 
-function readmenu(num) {
-  window.location.href = num;
-  const mobBtn = document.querySelector('.mobile-navlist');
-  mobBtn.style.display = 'none';
-}
+openMenu.addEventListener('click', show);
+mobBtn.addEventListener('click', show);
+linkBtn.addEventListener('click', readmenu);
