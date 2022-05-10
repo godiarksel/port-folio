8d0f78d2901682e0d136ccb907a200f208614621
godfrey <godiarksel@gmail.com>
2022-05-10 16:19:55 +0300

    Create Html for pop-up

--- myscript.js
+++ myscript.js
@@ -1,17 +1,17 @@
 const mobBtn = document.querySelector('.mobile-navlist');
 const linkBtn = document.querySelector('.mobile-navlist a');
+const openMenu = document.querySelector('.openMenu');
+
+function show() {
+  openMenu.style.opacity = '0';
+  mobBtn.style.left = '0';
+}
 
 function readmenu(num) {
   window.location.href = num;
   // const mobBtn = document.querySelector('.mobile-navlist');
   mobBtn.style.display = 'none';
-}
-
-const openMenu = document.querySelector('.openMenu');
-
-function show() {
-  openMenu.style.display = 'none';
-  mobBtn.style.left = '0';
+  openMenu.style.opacity = '1';
 }
 
 openMenu.addEventListener('click', show);
