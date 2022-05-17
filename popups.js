2fb39406089f63fb83ac2425141aedb02d1b64dc
t-bello7 <bello4aus@gmail.com>
2022-05-17 14:54:39 +0100

    fix: the dynamic popup card

--- popups.js
+++ popups.js
@@ -1,20 +1,101 @@
-const modalBtn = document.querySelectorAll('#modal-open');
-const closeModal = document.querySelector('#modal-close');
 const modalOverlay = document.querySelector('#modal-overlay');
-const modal = document.querySelector('.modal');
+const workSection = document.querySelector('#work-section');
 
-function openModal() {
-  modal.style.display = 'block';
-  modalOverlay.style.display = 'block';
-}
+workSection.addEventListener('click', (e)=> {
+  e.preventDefault()
 
-function closePop() {
-  modal.style.display = 'none';
-  modalOverlay.style.display = 'none';
-}
-for (let j = 0; j < modalBtn.length; j += 1) {
-  modalBtn[j].addEventListener('click', openModal);
-}
+  const modalBtn = e.target.closest('.modal-open');
+  
+  if (!modalBtn) return;
 
-modalOverlay.onclick = closePop;
-closeModal.onclick = closePop;
+  const modal = modalBtn.parentNode.parentNode.parentNode.nextElementSibling
+  
+  modal.classList.add('is-open');
+  modalOverlay.classList.add('is-open');
+  const closeBtn = document.querySelectorAll('.modal-close');
+    
+  for (let i = 0; i < closeBtn.length; i++){
+    closeBtn[i].addEventListener('click', () =>{
+      modal.classList.remove('is-open');
+      modalOverlay.classList.remove('is-open');
+    })
+  }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export function openModal(id) {
+//   console.log('redners first');
+//   for (let work of works ) {
+//     if ( work['id'] == id){
+//       workSection.innerHTML += `
+//     <div class="modal">
+//       <div>
+//         <div class="modal_heading">
+//           <h3>${work.name}</h3>
+//         <img id="modal-close" src="images/cancel-icon2.png" alt="cancel-icon">
+//       </div>
+    
+//       <div class="canopy-container">
+//         <ul>
+//           <li class="canopy-header"><p>${work.role[0]}</p></li>  
+//           <li class="canopy-img"><img src="images/counter.png" alt="counter"></li>
+//           <li class="canopy-text">${work.role[1]}</li>
+//           <li class="canopy-img"><img src="images/counter.png" alt="counter"></li>
+//           <li class="canopy-text">${work.role[2]}</li>
+//         </ul>
+//       </div>
+//       <div class="modal_image"><img src="${work.image}" alt="${work.alt[0]}"></div>
+//       <div class="desk-modal__content">
+//       <div class="tonic-parag">
+//         <p>
+//         ${work.content}
+//         </p>
+//       </div>
+//       <div class="desk-modal__content__iconsbtn">
+//       <div class="tonic-icons-container">
+//         <ul>
+//           <li><img src="${work.technologies[0]}" alt="${work.alt[1]}"></li>
+//           <li><img src="${work.technologies[1]}" alt="${work.alt[2]}"></li> 
+//           <li><img src="${work.technologies[2]}" alt="${work.alt[3]}"></li> 
+//         </ul>
+//       </div>
+//       <div class="modal_btn">
+//         <div class="tonic-btn">
+//         <button id = "liveBtn" type="button">See Live
+//         </button>
+//       </div>
+//       <div class="tonic-btn">
+//         <button id="sourceBtn" type="button"> See Source
+//           </button>
+//       </div>
+//     </div>
+//  </div>
+//  </div>
+//   </div> `;
+//     }
+//   }
+// }
+
+
--- projectcards.js
+++ projectcards.js
@@ -1,61 +1,14 @@
+import { works } from "./works.js";
 const workSection = document.querySelector('#work-section');
+const workCards = works;
 
-const workCards = [
-  {
-    container: 'tonic-container',
-    image: 'images/tonic-portfolio.png',
-    name: 'Tonic',
-    role: ['CANOPY', 'Back End Dev', 2015],
-    description: 'A daily selection of privately personalized reads no accounts or sign-ups required.',
-    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    technologies: ['images/html-tag.png', 'images/css-tag.png', 'images/javascript-tag.png'],
-    liveLink: 'https://godiarksel.github.io/port-folio/',
-    sourceLink: 'https://github.com/godiarksel/port-folio',
-    alt: ['tonic', 'html-tag', 'css-tag', 'javascript-tag'],
-  },
-  {
-    container: 'tonic-container0',
-    image: 'images/multi-post-portfolio.png',
-    name: 'Multi-Post Stories',
-    role: ['CANOPY', 'Back End Dev', 2015],
-    description: 'A daily selection of privately personalized reads no accounts or sign-ups required.',
-    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    technologies: ['images/html-tag.png', 'images/css-tag.png', 'images/javascript-tag.png'],
-    liveLink: 'https://godiarksel.github.io/port-folio/',
-    sourceLink: 'https://github.com/godiarksel/port-folio',
-    alt: ['multi-post-portfolio', 'html-tag', 'css-tag', 'javascript-tag'],
-  },
-  {
-    container: 'tonic-container',
-    image: 'images/tonic2.png',
-    name: 'Tonic',
-    role: ['CANOPY', 'Back End Dev', 2015],
-    description: 'A daily selection of privately personalized reads no accounts or sign-ups required.',
-    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    technologies: ['images/html-tag.png', 'images/css-tag.png', 'images/javascript-tag.png'],
-    liveLink: 'https://godiarksel.github.io/port-folio/',
-    sourceLink: 'https://github.com/godiarksel/port-folio',
-    alt: ['tonic', 'html-tag', 'css-tag', 'javascript-tag'],
-  },
-  {
-    container: 'tonic-container0',
-    image: 'images/multi-post-stories2.png',
-    name: 'Multi-Post Stories',
-    role: ['CANOPY', 'Back End Dev', 2015],
-    description: 'A daily selection of privately personalized reads no accounts or sign-ups required.',
-    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    technologies: ['images/html-tag.png', 'images/css-tag.png', 'images/javascript-tag.png'],
-    liveLink: 'https://godiarksel.github.io/port-folio/',
-    sourceLink: 'https://github.com/godiarksel/port-folio',
-    alt: ['multi-post-stories2', 'html-tag', 'css-tag', 'javascript-tag'],
-  },
-];
 let cards = '';
+
 for (let i = 0; i < workCards.length; i += 1) {
   cards += `
   <div class="${workCards[i].container}">
-  <div class="tonic-container1"><img src="${workCards[i].image}" alt="${workCards[i].alt[0]}"></div>
-  <div class="tonic-container2">
+    <div class="tonic-container1"><img src="${workCards[i].image}" alt="${workCards[i].alt[0]}"></div>
+    <div class="tonic-container2">
       <h3 class="tonic-heading">${workCards[i].name}</h3>
       <div class="canopy-container">
           <ul>
@@ -64,7 +17,7 @@ for (let i = 0; i < workCards.length; i += 1) {
           <li class="canopy-text">${workCards[i].role[1]}</li>
           <li class="canopy-img"><img src="images/counter.png" alt="counter"></li>
           <li class="canopy-text">${workCards[i].role[2]}</li>
-      </ul>
+          </ul>
       </div>
       <div class="tonic-parag">
           <p>
@@ -79,55 +32,59 @@ for (let i = 0; i < workCards.length; i += 1) {
          </ul>
       </div>
       <div class="tonic-btn">
-          <button id="modal-open" type="button">See Project</button>
+          <button id="modal-open" class="modal-open" type="button">See Project</button>
       </div>
-    </div>
-    </div>
+      </div>
 
-    <div class="modal">
-    <div>
-        <div class="modal_heading">
-         <h3>${workCards[i].name}</h3>
-         <img id="modal-close" src="images/cancel-icon2.png" alt="cancel-icon">
-        </div>
-        
-        <div class="canopy-container">
-            <ul>
-            <li class="canopy-header"><p>${workCards[i].role[0]}</p></li>  
-            <li class="canopy-img"><img src="images/counter.png" alt="counter"></li>
-            <li class="canopy-text">${workCards[i].role[1]}</li>
-            <li class="canopy-img"><img src="images/counter.png" alt="counter"></li>
-            <li class="canopy-text">${workCards[i].role[2]}</li>
-        </ul>
-        </div>
-        <div class="modal_image"><img src="${workCards[i].image}" alt="${workCards[i].alt[0]}"></div>
-        <div class="desk-modal__content">
-        <div class="tonic-parag">
-            <p>
-            ${workCards[i].content}
-            </p>
-        </div>
-        <div class="desk-modal__content__iconsbtn">
-        <div class="tonic-icons-container">
-            <ul>
-            <li><img src="${workCards[i].technologies[0]}" alt="${workCards[i].alt[1]}"></li>
-            <li><img src="${workCards[i].technologies[1]}" alt="${workCards[i].alt[2]}"></li> 
-            <li><img src="${workCards[i].technologies[2]}" alt="${workCards[i].alt[3]}"></li> 
-           </ul>
-        </div>
-        <div class="modal_btn">
-        <div class="tonic-btn">
-            <button id = "liveBtn" type="button">See Live
-            </button>
-        </div>
-        <div class="tonic-btn">
-        <button id="sourceBtn" type="button"> See Source
-        </button>
-       </div>
-        </div>
-     </div>
-     </div>
       </div>
-    </div>  `;
+
+        <div class="modal">
+      <div>
+        <div class="modal_heading">
+          <h3>${workCards[i].name}</h3>
+        <img id="modal-close" class="modal-close" src="images/cancel-icon2.png" alt="cancel-icon">
+      </div>
+    
+      <div class="canopy-container">
+        <ul>
+          <li class="canopy-header"><p>${workCards[i].role[0]}</p></li>  
+          <li class="canopy-img"><img src="images/counter.png" alt="counter"></li>
+          <li class="canopy-text">${workCards[i].role[1]}</li>
+          <li class="canopy-img"><img src="images/counter.png" alt="counter"></li>
+          <li class="canopy-text">${workCards[i].role[2]}</li>
+        </ul>
+      </div>
+      <div class="modal_image"><img src="${workCards[i].image}" alt="${workCards[i].alt[0]}"></div>
+      <div class="desk-modal__content">
+      <div class="tonic-parag">
+        <p>
+        ${workCards[i].content}
+        </p>
+      </div>
+      <div class="desk-modal__content__iconsbtn">
+      <div class="tonic-icons-container">
+        <ul>
+          <li><img src="${workCards[i].technologies[0]}" alt="${workCards[i].alt[1]}"></li>
+          <li><img src="${workCards[i].technologies[1]}" alt="${workCards[i].alt[2]}"></li> 
+          <li><img src="${workCards[i].technologies[2]}" alt="${workCards[i].alt[3]}"></li> 
+        </ul>
+      </div>
+      <div class="modal_btn">
+        <div class="tonic-btn">
+        <button id = "liveBtn" type="button">See Live
+        </button>
+      </div>
+      <div class="tonic-btn">
+        <button id="sourceBtn" type="button"> See Source
+          </button>
+      </div>
+    </div>
+      </div>
+  </div>
+  </div>
+  
+    </div> 
+   `;
 }
+
 workSection.innerHTML += cards;
