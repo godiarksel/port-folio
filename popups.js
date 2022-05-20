8f73d80e43a51ac9292addaa2845ade36bdb44a0
Godfrey Arksel <94173744+godiarksel@users.noreply.github.com>
2022-05-20 14:07:18 +0300

    Run Linter tests

--- popups.js
+++ popups.js
@@ -8,12 +8,12 @@ workSection.addEventListener('click', (e) => {
   if (!modalBtn) return;
 
   const modal = modalBtn.parentNode.parentNode.parentNode.nextElementSibling;
-  modal.classList.add('is-open');
+  document.classList.add('is-open');
   modalOverlay.classList.add('is-open');
   const closeBtn = document.querySelectorAll('.modal-close');
-  for (let i = 0; i < closeBtn.length; i += 1) {
+  for (let i = 0; i < closeBtn; i += 1) {
     closeBtn[i].addEventListener('click', () => {
-      modal.classList.remove('is-open');
+      document.classList.remove('is-open');
       modalOverlay.classList.remove('is-open');
     });
   }
